71d8cfebe0a0be7a768f95c9e75c332e27c5df00
Terry <misterfreemann@gmail.com>
2023-04-26 15:35:55 -0400

    added styling, added Readme,minor bugs fixed

--- README.md
+++ README.md
@@ -1,1 +1,70 @@
 # Tech-Blog
+
+## Description
+
+This is a Tech Blog web app designed to create, update and delete posts on any tech field as well as leave comments.<br/>
+
+
+## Table of contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contribution](#contribution)
+- [Questions](#questions)
+- [License](#license)
+
+## Installation
+
+
+- Clone application [Tech-Blog](https://github.com/TerryKor/Tech-Blog)<br />
+
+- Create `.env` file in derictory using this sample:<br />
+```
+DB_NAME='tech_blog_db'(database name)
+DB_PASSWORD=''(password)
+DB_USER=''(username)
+```
+- Then in file directory, to install necessary dependencies, run the following command:
+```
+npm i
+```
+- To seed the data, run the following command:
+```
+node ./seeds/index.js
+```
+- To run the app run the following command:<br />
+```
+node server.js
+```
+
+- Note: please make sure you have installed [MySQL](https://www.mysql.com/downloads/) and created account;<br />
+
+- To create schema run the following commands:<br />
+to login to MYSQL and enter password when prompted, run the following command:<br />
+```
+mysql -u root -p (where "root" is your user name)
+```
+- then run:
+```
+source ./db/schema.sql 
+```
+
+## Usage
+
+- See deplpoyed page [here](). Otherwise follow Insatallation instructions<br />
+
+
+## Contribution
+
+Application was created by Terry Kornienko and if you want to contribute send me an email.<br />
+
+## Questions
+
+My Email:
+[misterfreemann@gmail.com](mailto:misterfreemann@gmail.com)
+My Github:
+[TerryKor](https://github.com/TerryKor)
+
+## License
+
+![badge](https://img.shields.io/badge/license-MIT-blue)
--- seeds/postData.js
+++ seeds/postData.js
@@ -3,11 +3,28 @@ const { Post } = require("../models");
 const postData = [
   {
     title: "Tailwind CSS",
-    post_content: "Tailwind CSS works by scanning all of your HTML files,JavaScript components, and any other templates for class names, generating the the corresponding styles and then writing them to a static CSS file."
+    content: "Tailwind CSS works by scanning all of your HTML files,JavaScript components, and any other templates for class names, generating the the corresponding styles and then writing them to a static CSS file.",
+    author_id:1,
   },
   {
     title: "Sequelize",
-    post_content: "Sequelize is a modern TypeScript and Node.js ORM for Oracle, Postgres, MySQL, MariaDB, SQLite and SQL Server, and more. Featuring solid transaction support, relations, eager and lazy loading, read replication and more."
+    content: "Sequelize is a modern TypeScript and Node.js ORM for Oracle, Postgres, MySQL, MariaDB, SQLite and SQL Server, and more. Featuring solid transaction support, relations, eager and lazy loading, read replication and more.",
+    author_id:2,
+  },
+  {
+    title: "Express",
+    content: "Express is a minimal and flexible Node.js web application framework that provides a robust set of features for web and mobile applications.",
+    author_id:1,
+  },
+  {
+    title: "MySQL2",
+    content: "MySQL2 project is a continuation of MySQL-Native. Protocol parser code was rewritten from scratch and api changed to match popular mysqljs/mysql. MySQL2 team is working together with mysqljs/mysql team to factor out shared code and move it under mysqljs organisation.",
+    author_id:2,
+  },
+  {
+    title: "React",
+    content: "React (also known as React.js or ReactJS) is a free and open-source front-end JavaScript library[3] for building user interfaces based on components. It is maintained by Meta (formerly Facebook) and a community of individual developers and companies",
+    author_id:1,
   },
 ];
 
